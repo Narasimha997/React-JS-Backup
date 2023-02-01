df50fb8d37f49f2ad15a1cb7f1cc2b4ae8523940
Narasimha997 <gkumar301099@gmail.com>
2023-02-01 21:34:45 +0530

    React JS

--- blue-lock/src/App.js
+++ blue-lock/src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
 import Greet from './component/Greet';
+import Welcome from './component/one';
 class App extends Component{
   render(){
   return (
     <div className="App">
         <Greet/> 
+        <Welcome/>
     </div>
   );
   }
