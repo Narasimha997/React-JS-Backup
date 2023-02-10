e3e27fd11b9325c886b90bcfa8be8787cdec5dc9
Narasimha997 <gkumar301099@gmail.com>
2023-02-10 12:03:57 +0530

    React JS

--- blue-lock/src/App.js
+++ blue-lock/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import Product from "./component-3/product.js/product-1";
+// import Product from "./component-3/product.js/product-1";
 // import Before from "./component-3/Hi";
 // import Ref from "./component-3/function";
 // import Flip from "./component-3/withoutJSX";
 // import Mi from "./component-3/class&props-2";
+import {BrowserRouter as Router,Route,Routes } from 'react-router-dom'
+import Navbar from "./navabar/Navbar";
+import Product from "./component-3/product.js/product-1";
 const App=()=>{
   return (
     <div>
@@ -11,7 +14,13 @@ const App=()=>{
       {/* <Ref/> */}
       {/* <Flip/> */}
       {/* <Mi/> */}
-      <Product/>
+      {/* <Product/> */}
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/blue-lock/src/component-3/product.js/product-1.js' element={<Product/>}/>
+        </Routes>
+      </Router>
     </div>
   )
 }
--- blue-lock/src/navabar/Navbar.js
+++ blue-lock/src/navabar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link} from 'react-router-dom'
 class Navbar extends Component{
     render(){
         return(
@@ -7,10 +8,10 @@ class Navbar extends Component{
              <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
                 <div className="ml-auto">
                     <ul className="navbar-nav ml-auto">
-                        <li className="nav-list"><a href="./about.html" className="nav-link">About</a></li>
-                        <li className="nav-list"><a href="./contact.html" className="nav-link">Contact</a></li>
-                        <li className="nav-list"><a href="./service.html" className="nav-link">Service</a></li>
-                        <li className="nav-list"><a href="./Product.html" className="nav-link">Product</a></li>
+                        <li className="nav-list"><Link to="/blue-lock/src/component-3/product.js/product-1.js" className="nav-link">Product</Link></li>
+                        <li className="nav-list"><Link to="./contact.html" className="nav-link">Contact</Link></li>
+                        <li className="nav-list"><Link to="./service.html" className="nav-link">Service</Link></li>
+                        <li className="nav-list"><Link to="./Product.html" className="nav-link">Product</Link></li>
                     </ul>
                 </div>
             </nav>
