fd2656179c0088b78532ef7611d7053bf09c1b0e
Narasimha997 <gkumar301099@gmail.com>
2023-02-20 11:23:33 +0530

    ok

--- blue-lock/src/App.js
+++ blue-lock/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import Hi from './component/greet-1'
 import One from './Revise/one'
 import Parent from './Revise/parent'
-import Product from './Revise/Product'
 import Two from './Revise/two'
 
 function App() {
@@ -11,8 +9,7 @@ function App() {
       <One/>
       <Two/>
       <Parent/>
-      <Hi/>
-      <Product/>
+  
     </div>
   )
 }
