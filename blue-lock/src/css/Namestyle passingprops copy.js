7ab0a7ec46a71b6c8eed7ed83febaee6185ea949
Narasimha997 <gkumar301099@gmail.com>
2023-02-21 23:02:56 +0530

    React JS

--- blue-lock/src/css/Namestyle passingprops copy.js
+++ blue-lock/src/css/Namestyle passingprops copy.js
@@ -5,7 +5,7 @@ function Namestyle1(props) {
   return (
     <div>
           <h1 className='resolve'> Error</h1>
-          <h1 className={Styles.reject}> Success</h1>
+          {/* <h1 className={Styles.reject}> Success</h1> */}
           {/* you cannot accidentally use a className is defined for some other component whenever we are using ".module.css" */}
 
     <h1 className={property}>One Piece is Real</h1>
