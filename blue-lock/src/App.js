d09f67241b7b02f819c5442e8e4526047bc42cc4
Narasimha997 <gkumar301099@gmail.com>
2023-02-24 23:38:29 +0530

    React JS@

--- blue-lock/src/App.js
+++ blue-lock/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 // import Namestyle from './css/Namestyle'
-import Namestyle1 from './css/Namestyle passingprops';
+// import Namestyle1 from './css/Namestyle passingprops';
 // import Names from './listrendering/indexaskey/Names'
 // import One from './Revise/one'
 import Parent from './Revise/index-1';
 // import Two from './Revise/two'
 import './css/appStyle.css';
 import Styles from './css/appStyle.module.css';
+import Form from './Form Handling/Controlled Component/email';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <h1 className={Styles.reject}> Success</h1>
       <Namestyle1 primary={true}/>
    */}
+   <Form/>
     </div>
   );
 }
